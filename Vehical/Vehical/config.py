a3085da824d41b777944a8a2a73450e2db97149e
Prayansh22002 <prayansh22002@gmail.com>
2025-05-26 16:04:10 +0530

    Delete Vehical/Vehical directory

--- Vehical/Vehical/config.py
+++
@@ -1,3 +0,0 @@
-# config.py
-DATABASE_URL = "postgresql://user_info_4gfk_user:Hv2nshMamC3p7C1sU9dNVJMJHrNikSmA@dpg-d0o1ltmuk2gs73fk4tmg-a.oregon-postgres.render.com/user_info_4gfk"
-SECRET_KEY = "dev_key"
